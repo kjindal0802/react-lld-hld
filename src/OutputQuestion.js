daceb725638afe815803e5e02a4e60b489270701
kjindal0802 <kjindal0802@gmail.com>
2024-05-14 06:54:15 +0530

    14052024-0654

--- src/OutputQuestion.js
+++ src/OutputQuestion.js
@@ -1,62 +1,74 @@
 var person = {
-    name: 'John',
-    greet: function() {
-      console.log('Hello, my name is ' + this.name);
-    }
-  };
-  
-  var greetFunction = person.greet;
-  greetFunction();
+  name: "John",
+  greet: function () {
+    console.log("Hello, my name is " + this.name);
+  },
+};
+
+var greetFunction = person.greet;
+greetFunction();
 
 //   Output: Hello, my name is undefined
 
-//   Explanation: When greetFunction is called, 
-//   it's invoked without any context, so this inside the greet function 
-//   becomes the global object (window in a browser environment). 
+//   Explanation: When greetFunction is called,
+//   it's invoked without any context, so this inside the greet function
+//   becomes the global object (window in a browser environment).
 //   However, the global object doesn't have a name property, hence this.name evaluates to undefined.
 // ----------------------------------------------------------------------------------------------------------------------------
 
 var person = {
-    name: 'Alice',
-    greet: function() {
-      setTimeout(function() {
-        console.log('Hello, my name is ' + this.name);
-      }, 1000);
-    }
-  };
-  
-  person.greet();
+  name: "Alice",
+  greet: function () {
+    setTimeout(function () {
+      console.log("Hello, my name is " + this.name);
+    }, 1000);
+  },
+};
+
+person.greet();
 // ----------------------------------------------------------------------------------------------------------------------------
 
 //   Output: Hello, my name is undefined
 
-// Explanation: Inside the setTimeout callback function, 
-// this refers to the global object (window), not the person object. 
+// Explanation: Inside the setTimeout callback function,
+// this refers to the global object (window), not the person object.
 // Therefore, this.name evaluates to undefined.
-  
+
+var person = {
+  name: "Alice",
+  greet: function () {
+    setTimeout(() => {
+      console.log("Hello, my name is " + this.name);
+    }, 1000);
+  },
+};
+
+person.greet();
+
+// ----------------------------------------------------------------------------------------------------------------------------
+
 function Person(name) {
-    this.name = name;
-  }
-  
-  Person.prototype.greet = function() {
-    console.log('Hello, my name is ' + this.name);
-  };
-  
-  var john = new Person('John');
-  var greet = john.greet;
-  greet();
-  
+  this.name = name;
+}
+
+Person.prototype.greet = function () {
+  console.log("Hello, my name is " + this.name);
+};
+
+var john = new Person("John");
+var greet = john.greet;
+greet();
 
 //   Output: Hello, my name is undefined
 
-// Explanation: Similar to the first question, 
-// when greet is invoked without any context, this inside the greet 
-// function refers to the global object (window). Since the global object 
+// Explanation: Similar to the first question,
+// when greet is invoked without any context, this inside the greet
+// function refers to the global object (window). Since the global object
 // doesn't have a name property, this.name evaluates to undefined.
 
-var button = document.getElementById('myButton');
+var button = document.getElementById("myButton");
 
-button.addEventListener('click', function() {
+button.addEventListener("click", function () {
   console.log(this);
 });
 
@@ -66,50 +78,48 @@ button.addEventListener('click', function() {
 //  So when the button is clicked, this inside the event listener function refers to the button element.
 
 var obj = {
-    foo: function() {
+  foo: function () {
+    console.log(this === obj);
+    setTimeout(function () {
       console.log(this === obj);
-      setTimeout(function() {
-        console.log(this === obj);
-      }, 0);
-    }
-  };
-  obj.foo();
+    }, 0);
+  },
+};
+obj.foo();
 
 //   true
 // false
 
-
-//   Explanation: Inside the foo method, 
-//   this refers to the obj object. However, inside the setTimeout callback function, 
+//   Explanation: Inside the foo method,
+//   this refers to the obj object. However, inside the setTimeout callback function,
 //   this refers to the global object (window). This happens because regular functions defined with
 //   function keyword have their own this binding, unlike arrow functions.
-  
 
 var person = {
-    name: 'Alice',
-    greet: function() {
-      console.log('Hello, my name is ' + this.name);
-    }
-  };
-  
-  var boundGreet = person.greet.bind({ name: 'Bob' });
-  boundGreet();
+  name: "Alice",
+  greet: function () {
+    console.log("Hello, my name is " + this.name);
+  },
+};
+
+var boundGreet = person.greet.bind({ name: "Bob" });
+boundGreet();
 
 //   Output: Hello, my name is Bob
 
-// Explanation: The bind method creates a new function where this is explicitly set to the object passed 
-// as an argument. So in this case, boundGreet is a function where this is set to an object with name property 
+// Explanation: The bind method creates a new function where this is explicitly set to the object passed
+// as an argument. So in this case, boundGreet is a function where this is set to an object with name property
 // equal to 'Bob'. Therefore, when boundGreet is invoked, it logs 'Hello, my name is Bob'.
 
 var obj = {
-    x: 10,
-    getX: function() {
-        console.log(this.x)
-        function inner() {
-            console.log(this.x);
-        }
-        inner();
+  x: 10,
+  getX: function () {
+    console.log(this.x);
+    function inner() {
+      console.log(this.x);
     }
+    inner();
+  },
 };
 obj.getX();
 
@@ -126,14 +136,14 @@ obj.getX();
 // Because there is no global variable x defined, this.x inside inner results in undefined being logged.
 
 var obj = {
-    x: 10,
-    getX: function() {
-        console.log(this.x)
-        const inner = () => {
-            console.log(this.x);
-        }
-        inner();
-    }
+  x: 10,
+  getX: function () {
+    console.log(this.x);
+    const inner = () => {
+      console.log(this.x);
+    };
+    inner();
+  },
 };
 obj.getX();
 
@@ -145,12 +155,11 @@ obj.getX();
 // getX is still called as a method of obj, so within getX, this refers to obj.
 // this.x is 10, so 10 is logged to the console.
 // inner() function call using an arrow function:
-// inner is now defined as an arrow function. Arrow functions do not have their own this context; 
+// inner is now defined as an arrow function. Arrow functions do not have their own this context;
 // instead, they inherit this from the enclosing execution context.
 // Since inner is defined within getX where this refers to obj, the inherited this in inner also refers to obj.
 // As a result, this.x inside inner is still 10, and 10 is logged again.
 // ----------------------------------------------------------------------------------------------------------------------------
-
 
 var obj1 = { x: 10 };
 var obj2 = { x: 10 };
@@ -164,8 +173,8 @@ console.log(obj1 === obj2);
 
 let arr = [1, 2, 3];
 function changeArray(array) {
-    array.push(4);
-    array = [4, 5, 6];
+  array.push(4);
+  array = [4, 5, 6];
 }
 changeArray(arr);
 console.log(arr);
@@ -197,12 +206,11 @@ console.log(originalArray);
 // Output: Therefore, when console.log(originalArray); is called, it outputs [1, 2, [100, 4]] because the nested array shared between originalArray and shallowCopy was modified through shallowCopy.
 // ----------------------------------------------------------------------------------------------------------------------------
 
-
 var a = 10;
-function test(){
+function test() {
   a++;
   console.log(a);
-  var a  = 20;
+  var a = 20;
   console.log(a);
 }
 test();
@@ -210,60 +218,55 @@ console.log(a);
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-
-
-Array(1,2,3,4,5).forEach(v => {
-    Promise.resolve().then(() => console.log('Promise:' + v));
-    console.log('Console:' + v);
-    setTimeout(() => console.log('TimeOut:' + v));
+Array(1, 2, 3, 4, 5).forEach((v) => {
+  Promise.resolve().then(() => console.log("Promise:" + v));
+  console.log("Console:" + v);
+  setTimeout(() => console.log("TimeOut:" + v));
 });
 // ----------------------------------------------------------------------------------------------------------------------------
 
-
-
 const firstPromise = new Promise((res, rej) => {
-    setTimeout(res, 500, 'one');
+  setTimeout(res, 500, "one");
 });
 
 const secondPromise = new Promise((res, rej) => {
-    setTimeout(res, 100, 'two');
+  setTimeout(res, 100, "two");
 });
 
-Promise.race([firstPromise, secondPromise]).then(res => console.log(res));
+Promise.race([firstPromise, secondPromise]).then((res) => console.log(res));
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-const myPromise = (text) => Promise.resolve('I have resolved ' + text);
- 
+const myPromise = (text) => Promise.resolve("I have resolved " + text);
+
 function firstFunction() {
-    myPromise('first').then(res => console.log(res));
-    console.log('first');
+  myPromise("first").then((res) => console.log(res));
+  console.log("first");
 }
- 
+
 async function secondFunction() {
-    console.log(await myPromise('second'));
-    console.log('second');
+  console.log(await myPromise("second"));
+  console.log("second");
 }
- 
+
 firstFunction();
 secondFunction();
-
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
 console.log("a");
-setTimeOut(()=>{
-conosle.log("b")
-},0);
-console.log("c")
+setTimeOut(() => {
+  conosle.log("b");
+}, 0);
+console.log("c");
 // ----------------------------------------------------------------------------------------------------------------------------
 
-let a = "Y"
+let a = "Y";
 let b = {
-  c : "N"
-}
-let d = {}
-console.log( a === "Y" ? d.c !== "Y"  : b.c  !== "N")
+  c: "N",
+};
+let d = {};
+console.log(a === "Y" ? d.c !== "Y" : b.c !== "N");
 // ----------------------------------------------------------------------------------------------------------------------------
 
 const map = new Map();
@@ -276,107 +279,116 @@ map.set("apple", 7);
 console.log(map.get("apple"));
 // ----------------------------------------------------------------------------------------------------------------------------
 
-(function() {
- let x = 10;
-function changeX(value) {
-  value = 20;
- }
-changeX(x);
-console.log(x);
+(function () {
+  let x = 10;
+  function changeX(value) {
+    value = 20;
+  }
+  changeX(x);
+  console.log(x);
 })();
 // ----------------------------------------------------------------------------------------------------------------------------
 
 var a = 1;
 function x(params) {
- console.log(a)
-   if(a) var a = 9;
-   console.log(a)
+  console.log(a);
+  if (a) var a = 9;
+  console.log(a);
 }
-x(10)
+x(10);
 // ----------------------------------------------------------------------------------------------------------------------------
 
+const p = new Promise((res, rej) => {
+  console.log("yehh");
+  rej(1);
+});
 
-const p  = new Promise((res,rej)=>{
-    console.log('yehh')
-    rej(1)
-})
- 
-p.then((v)=>{
-    console.log('resolved')
+p.then(
+  (v) => {
+    console.log("resolved");
     return v;
-},(e)=>{
-    console.log('rejected')
-    return e
-}).then((v)=>{
-    console.log('resolved')
-},(e)=>{
-    console.log('rejected')
-})
-p.then((v)=>{
-    console.log('resolved')
+  },
+  (e) => {
+    console.log("rejected");
+    return e;
+  }
+).then(
+  (v) => {
+    console.log("resolved");
+  },
+  (e) => {
+    console.log("rejected");
+  }
+);
+p.then(
+  (v) => {
+    console.log("resolved");
     return v;
-},(e)=>{
-    console.log('rejected')
-    return e
-}).then((v)=>{
-    console.log('resolved')
-},(e)=>{
-    console.log('rejected')
-})
+  },
+  (e) => {
+    console.log("rejected");
+    return e;
+  }
+).then(
+  (v) => {
+    console.log("resolved");
+  },
+  (e) => {
+    console.log("rejected");
+  }
+);
 // ----------------------------------------------------------------------------------------------------------------------------
 
-
-
-const promise = new Promise(res => res(2))
-promise.then(v => {
-	console.log(v);
-  return v*2
-}).then(v => {
-	console.log(v);
-  return v*2;
-}).finally(v => {
-	console.log(v);
-  return v*2;
-}).then(v => {
-	console.log(v);
-})
+const promise = new Promise((res) => res(2));
+promise
+  .then((v) => {
+    console.log(v);
+    return v * 2;
+  })
+  .then((v) => {
+    console.log(v);
+    return v * 2;
+  })
+  .finally((v) => {
+    console.log(v);
+    return v * 2;
+  })
+  .then((v) => {
+    console.log(v);
+  });
 // ----------------------------------------------------------------------------------------------------------------------------
-
 
 const a = {
-    property1:123,
-    property2:()=>{
-      return "hello";
-    },
-    property3:"reactjs",
-    property4: null,
-    property5: undefined,
-    property6:NaN,
-    property1:456,
-  }
-  console.log(JSON.stringify(a));
+  property1: 123,
+  property2: () => {
+    return "hello";
+  },
+  property3: "reactjs",
+  property4: null,
+  property5: undefined,
+  property6: NaN,
+  property1: 456,
+};
+console.log(JSON.stringify(a));
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
-
 const p1 = Promise.reject(0);
-const p2 = new Promise(res => setTimeout(res, 1000, 'quick'));
-const p3 = new Promise(res => setTimeout(res, 500, 'slow'));
+const p2 = new Promise((res) => setTimeout(res, 1000, "quick"));
+const p3 = new Promise((res) => setTimeout(res, 500, "slow"));
 const promises = [p1, p2, p3];
- 
-Promise.any(promises).then(v => console.log(v, '1'));
-Promise.all(promises).then(v => console.log(v, '2'));
-Promise.allSettled(promises).then(v => console.log(v, '3'));
-Promise.race(promises).then(v => console.log(v, '4'));
 
+Promise.any(promises).then((v) => console.log(v, "1"));
+Promise.all(promises).then((v) => console.log(v, "2"));
+Promise.allSettled(promises).then((v) => console.log(v, "3"));
+Promise.race(promises).then((v) => console.log(v, "4"));
 
 // ----------------------------------------------------------------------------------------------------------------------------
 
 var a = 5;
 function x() {
-	var a = 10;
+  var a = 10;
   console.log(a);
-
 }
 x();
 console.log(a);
@@ -386,46 +398,46 @@ console.log(a);
 for (var i = 0; i < 11; i++) {
   setTimeout(() => console.log(i), 1000);
 }
-console.log('Learn');
+console.log("Learn");
 
 // ----------------------------------------------------------------------------------------------------------------------------
 // ----------------------------------------------------------------------------------------------------------------------------
 
 setTimeout(() => {
-console.log(x)
-console.log(y)
-}, 3000)
-var x = 10
-let y = 12
+  console.log(x);
+  console.log(y);
+}, 3000);
+var x = 10;
+let y = 12;
 // ----------------------------------------------------------------------------------------------------------------------------
 
 var a = 10;
 {
-var a = 20;
-console.log(a);
+  var a = 20;
+  console.log(a);
 }
-console.log(a)
+console.log(a);
 // ----------------------------------------------------------------------------------------------------------------------------
 
-sum(2)(3) & sum(2,3)
+sum(2)(3) & sum(2, 3);
 // ----------------------------------------------------------------------------------------------------------------------------
 
-var a = 10
+var a = 10;
 
 function one() {
-    console.log(a)
+  console.log(a);
 }
- 
+
 function two() {
-    var a = 20;
-    one();
+  var a = 20;
+  one();
 }
- 
-two()
+
+two();
 // ----------------------------------------------------------------------------------------------------------------------------
 
 let array1 = [1, 2];
- let array2 = [3, 4];
+let array2 = [3, 4];
 let a = array1.push(array2);
 
 console.log(a);
